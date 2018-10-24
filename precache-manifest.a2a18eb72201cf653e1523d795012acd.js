9d6f8240ee9122b539072579b5f3f102b31cb87a
Paula Hoare <paulahoare@gmail.com>
2018-10-23 20:46:37 -0400

    Updates

---
+++ precache-manifest.a2a18eb72201cf653e1523d795012acd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d25adc3a1d8b913b2e664ee181beaae",
+    "url": "/brain/static/media/brain_logo.8d25adc3.svg"
+  },
+  {
+    "revision": "c95ef5482671cba31dfd56440e091b7d",
+    "url": "/brain/static/media/Brain_Journal.c95ef548.xml"
+  },
+  {
+    "revision": "befc8c729aa8cacbee43",
+    "url": "/brain/static/js/runtime~main.befc8c72.js"
+  },
+  {
+    "revision": "c4ba9f4faa0f7509e6da",
+    "url": "/brain/static/js/main.c4ba9f4f.chunk.js"
+  },
+  {
+    "revision": "ea89600a1f31acc5a84b",
+    "url": "/brain/static/js/1.ea89600a.chunk.js"
+  },
+  {
+    "revision": "c4ba9f4faa0f7509e6da",
+    "url": "/brain/static/css/main.ca2e8a71.chunk.css"
+  },
+  {
+    "revision": "effcca9273c3d7f441393912accf97e0",
+    "url": "/brain/index.html"
+  }
+];
